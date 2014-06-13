6e3c129a835b014b929b053f093e7b71223308c1
ajoys <alvin.joys@gmail.com>
2014-06-12 20:12:52 -0400

    Added step
    Added step counter using FSM.

--- src/ca/uwaterloo/lab2_203_03/GeneralSensorEventListener.java
+++ src/ca/uwaterloo/lab2_203_03/GeneralSensorEventListener.java
@@ -6,6 +6,9 @@ import android.content.Context;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -16,11 +19,17 @@ public class GeneralSensorEventListener implements SensorEventListener{
 	private float xMaxValueAccel = 0;
 	private float yMaxValueAccel = 0;
 	private float zMaxValueAccel =0;
+	private float mNumOfAverage = 0;
+	private float mAverageMax = 0;
+	private float mCurrentMax = 0;
+	private float mAverageMin = 0;
+	private float mCurrentMin = 0;
+	private float TESTMIN;
 	private float x;
 	private float y;
 	private float z;
-	private float maxVal = 0;
-	private float minVal = 0;
+	private float maxAmp = (float)2.0;
+	private float minAmp = (float)-0.4;
 	private int stepCounter;
 	private boolean hasStepped = false;
 	// values for the FSM
@@ -28,7 +37,9 @@ public class GeneralSensorEventListener implements SensorEventListener{
 	private final int mRising = 1;
 	private final int mPeak = 2;
 	private final int mFalling = 3;
-	private final int mNegative = 4;
+	private final int mNegativeFall = 4;
+	private final int mNegativeMin = 5;
+	private final int mNegativeRising = 6;
 	//current state instantiated at rest
 	private int currentState = 0;
 	private float zPrev = 0;
@@ -52,6 +63,19 @@ public class GeneralSensorEventListener implements SensorEventListener{
 		mTvGen = new TextView(context);
 		mTvGen.setText(sensorType + value);
 		layout.addView(mTvGen);
+		Button resetMax = new Button(context);
+		resetMax.setText("RESET");
+		layout.addView(resetMax);
+		resetMax.setOnClickListener(new OnClickListener() {
+		    public void onClick(View v)
+		    {
+		    	xMaxValueAccel = 0;
+		    	yMaxValueAccel = 0;
+		    	zMaxValueAccel =0;
+		    	stepCounter = 0;
+		    	mNumOfAverage = 0;
+		    } 
+		});
 	}
 	@Override
 	public void onAccuracyChanged(Sensor sensor, int accuracy) {
@@ -64,9 +88,9 @@ public class GeneralSensorEventListener implements SensorEventListener{
 		//Accelerometer
 		if (se.sensor.getType() == Sensor.TYPE_LINEAR_ACCELERATION){
 			
-			x += (se.values[0] - x)/3;
-			y += (se.values[1] - y)/3;
-			z += (se.values[2] - z)/3;
+			x += (se.values[0] - x)/5;
+			y += (se.values[1] - y)/5;
+			z += (se.values[2] - z)/5;
 			
 			// Storing smoothed values into array
 			float[] values = new float[]{x, y, z};
@@ -74,46 +98,88 @@ public class GeneralSensorEventListener implements SensorEventListener{
 			//adding points to graph
 			graph.addPoint(values);
 			
-			//Updating max value for accelerometer
-			if (xMaxValueAccel < Math.abs(x)){
-				xMaxValueAccel = Math.abs(x);
-			}
-			if (yMaxValueAccel < Math.abs(y)){
-				yMaxValueAccel = Math.abs(y);
-			}
-			if (zMaxValueAccel < Math.abs(z)){
-				zMaxValueAccel = Math.abs(z);
-			}
-			
 			String value = String.format("\nX:%.4f "
 					+ "\nY:%.4f"
 					+ "\nZ:%.4f \n", x, y, z);
 			
-			mTvGen.setText("----" + sensorType + "-----" + value + "\n\n Steps: " + stepCounter);
+			//updating max values to textView
+			String maxValues = String.format("\nX-Max:%.4f "
+					+ "\nY-Max:%.4f"
+					+ "\nZ-Max:%.4f \n", xMaxValueAccel, yMaxValueAccel, zMaxValueAccel);
 			
 			switch(currentState){
 				case mRest:
-					if (z - zPrev > 0.1){
-						stepCounter ++;
-						currentState =0;
-						zPrev = z;
+					if (z >= (0.1 * maxAmp)){
+						currentState = 1;
 					}
+					else currentState = 0;
+					break;
 				case mRising:
-					return;
+					if (z >= (0.9 * maxAmp)){
+						currentState = 2;
+					}
+					else if(z <= (0.1 * maxAmp)){
+						currentState = 0;
+					}
+					break;
 				case mPeak:
-					return;
+					if (z > mCurrentMax){
+						mCurrentMax = z;
+					}	
+					if (z <= (0.9 * maxAmp)){
+						currentState = 3;
+					}
+					
+					break;
 				case mFalling:
-					return;
-				case mNegative:
-					return;
+					if (z <= (maxAmp * 0.1)){
+						currentState = 4;
+					}
+				case mNegativeFall:
+					if (z < minAmp * 0.95){
+						currentState = 5;
+					}
+					break;
+				case mNegativeMin:
+					if (z < mCurrentMin){
+						mCurrentMin = z;
+					}	
+					if (z >= minAmp * 0.95){
+						currentState = 6;
+					}
+					break;
+				case mNegativeRising:
+					mNumOfAverage++;
+					mAverageMax = (mAverageMax + mCurrentMax)/mNumOfAverage ;
+					mCurrentMax = 0;
+					mAverageMin = (mAverageMin + mCurrentMin)/mNumOfAverage;
+					TESTMIN = mCurrentMin;
+					mCurrentMin = 0;
+					stepCounter++;
+		
+					if(mNumOfAverage == 5){
+						if (mAverageMax > 0.8){
+							maxAmp = mAverageMax;
+							mAverageMax = 0;
+							mNumOfAverage = 0;
+						}
+						else{
+							mAverageMax = 0;
+							mNumOfAverage = 0;
+						}
+						
+						if (mAverageMin < 0){
+							minAmp = mAverageMin;
+							mAverageMin = 0;
+						}
+			
+					}
+					currentState = 0;
+					break;
 			}
-			
-			//updating max values to textView
-			/*String maxValues = String.format("\nX-Max:%.4f "
-					+ "\nY-Max:%.4f"
-					+ "\nZ-Max:%.4f \n", xMaxValueAccel, yMaxValueAccel, zMaxValueAccel);*/
-			
-			
+			mTvGen.setText("----" + sensorType + "-----" + value + "\n\nSteps: " + stepCounter + "\n Average: " 
+					+ mAverageMax + "\n" + maxAmp + "\n" + mNumOfAverage + "\nCurrentState" + currentState + "\nMinAmp " + minAmp
+					+ "\nAverage Min" + mAverageMin +"\nCurrent: "  + TESTMIN );
 		}
 	}
 
