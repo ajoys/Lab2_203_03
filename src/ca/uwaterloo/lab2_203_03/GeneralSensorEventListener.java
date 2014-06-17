24b690b1dde209f3b897dfe4c3c3b493ae2e948e
Alex Henderson <a.j.henderson1@hotmail.com>
2014-06-16 21:50:21 -0400

    Fully commented

--- src/ca/uwaterloo/lab2_203_03/GeneralSensorEventListener.java
+++ src/ca/uwaterloo/lab2_203_03/GeneralSensorEventListener.java
@@ -99,45 +99,62 @@ public class GeneralSensorEventListener implements SensorEventListener{
 					+ "\nY-Max:%.4f"
 					+ "\nZ-Max:%.4f \n", xMaxValueAccel, yMaxValueAccel, zMaxValueAccel);
 			
+			//Switch statement to implement a FSM
 			switch(currentState){
+				//First state checking if the phone is a rest state eg. standing still
 				case mRest:
+					//If z value is 10% of the maxAmp a step is occurring
 					if (z >= (0.1 * maxAmp)){
 						currentState = 1;
 					}
+					//If the case condition is not met, resets the state to 0;
 					else currentState = 0;
-					break;
+					break;//Exiting Case
+				//Second state checking if the user has begun taking a step
 				case mRising:
+					//If the z value is at least 70% of the maxAmp then the step is approaching its peak
 					if (z >= (0.7 * maxAmp)){
 						if (y < 0.5 && x < 0.5){
 							currentState = 0;
 						}
 						else currentState = 2;
 					}
+					//If the case condition is not met, resets the state to 0;
 					else if(z <= (0.1 * maxAmp)){
 						currentState = 0;
 					}
-					break;
+					break;//Exiting Case
+				//Third state checking if the  accelerometer has reached a max peak of a step
 				case mPeak:
+					//Setting the new max peak if the newest peak is higher
 					if (z > mCurrentMax){
 						mCurrentMax = z;
 					}
-					
+					//If the z value is now 70% of the maxAmp then the peak has already occurred
 					if (z <= (0.7 * maxAmp)){
 						currentState = 3;
 					}
 					
-					break;
+					break;//Exiting Case
+				//Fourth state checking if the step is nearing completion because
+				//the accelerometer is approaching 0
 				case mFalling:
+					//If the z value is less than 10% of the max peak then the falling state is occurring
 					if (z <= (maxAmp * 0.1)){
 						currentState = 4;
 					}
+					break;//Exiting Case
+				//Fifth state checking if the step is completed when the accelerometer
+				//reaches a certain negative value
 				case mNegative:
-					if (z <=0){
+					//If the z value is less than -0.5 then the step has been completed and a step is counted
+					if (z <=-0.5){
 						mNumOfAverage++;
 						mAverageMax = (mAverageMax + mCurrentMax)/mNumOfAverage ;
 						mCurrentMax = 0;
 						stepCounter++;
 			
+						//Setting the new maxAmp every 5 steps
 						if(mNumOfAverage == 5){
 							if (mAverageMax > 0.7){
 								maxAmp = mAverageMax;
@@ -150,10 +167,12 @@ public class GeneralSensorEventListener implements SensorEventListener{
 							}
 				
 						}
+						//Reseting the state to 0 once a full step is completed
 						currentState = 0;
 					}
-					break;
+					break;//Exiting Case
 			}
+			//Outputting the date values
 			mTvGen.setText("----" + sensorType + "-----" + value + "\n\nSteps: " + stepCounter + "\n Average: " 
 					+ mAverageMax + "\n" + maxAmp + "\n" + mNumOfAverage + "\nCurrentState" + currentState );
 		}
